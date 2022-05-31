7ce15d614fee4f0e140c17fd2dbb7f1146b29733
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-05-31 21:06:07 +0300

    completed

---
+++ API_Northwind/API_Northwind/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API_Northwind.Models;
+
+namespace API_Northwind.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(db.Customers.ToList());
+        }
+    }
+}
