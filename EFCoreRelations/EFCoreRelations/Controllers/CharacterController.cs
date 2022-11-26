d42aec8c495eeb089df7f2efb0f095fac6ffffda
BerkBasat <berk.basat@bilgeadamakademi.com>
2022-11-26 20:43:32 +0300

    .

---
+++ EFCoreRelations/EFCoreRelations/Controllers/CharacterController.cs
@@ -0,0 +1,71 @@
+﻿using EFCoreRelations.Data;
+using EFCoreRelations.Dtos;
+using EFCoreRelations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelations.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>>Get(int userId)
+        {
+            var characters = await _context.Characters
+                .Where(x => x.UserId == userId)
+                .Include(x => x.Weapon)
+                .ToListAsync();
+            return characters;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Add(CreateCharacterDto request)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            if (user == null)
+                return NotFound();
+
+            var newCharacter = new Character
+            {
+                Name = request.Name,
+                RpgClass = request.RpgClass,
+                User = user
+            };
+
+            _context.Characters.Add(newCharacter);
+            await _context.SaveChangesAsync();
+
+            return await Get(newCharacter.UserId);
+        }
+
+        [HttpPost("weapon")]
+        public async Task<ActionResult<Character>> AddWeapon(CreateWeaponDto request)
+        {
+            var character = await _context.Characters.FindAsync(request.CharacterId);
+            if (character == null)
+                return NotFound();
+
+            var newWeapon = new Weapon
+            {
+                Name = request.Name,
+                Damage = request.Damage,
+                Character = character
+            };
+
+            _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+    }
+}
---
+++ EFCoreRelations/EFCoreRelations/Models/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EFCoreRelations.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string RpgClass { get; set; } = "Knight";
+
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public User User { get; set; }
+        public Weapon Weapon { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
