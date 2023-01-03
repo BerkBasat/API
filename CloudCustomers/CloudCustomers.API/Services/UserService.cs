1479d511cbe455d7d5c2b2babd6c438ccb5e9b83
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-01-03 13:40:26 +0300

    .

---
+++ CloudCustomers/CloudCustomers.API/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using CloudCustomers.API.Config;
+using CloudCustomers.API.Models;
+using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace CloudCustomers.API.Services
+{
+    public interface IUserService
+    {
+        public Task<List<User>> GetAllUsers();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly UserApiOptions _apiConfig;
+
+        public UserService(HttpClient httpClient, IOptions<UserApiOptions> apiConfig)
+        {
+            _httpClient = httpClient;
+            _apiConfig = apiConfig.Value;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            var response = await _httpClient.GetAsync(_apiConfig.Endpoint);
+
+            if(response.StatusCode == HttpStatusCode.NotFound)
+                return new List<User> { };
+
+            var responseContent = response.Content;
+            var allUsers = await responseContent.ReadFromJsonAsync<List<User>>();
+            return allUsers.ToList();
+        }
+    }
+}
---
+++ CloudCustomers/CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,48 @@
+﻿using CloudCustomers.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() => new() {
+                new User
+                {
+                    Name = "Test1",
+                    Email = "test1@gmail.com",
+                    Address = new Address()
+                    {
+                        Street = "Test",
+                        City = "Istanbul",
+                        ZipCode = "34841"
+                    }
+                },
+                new User
+                {
+                    Name = "Test2",
+                    Email = "test2@gmail.com",
+                    Address = new Address()
+                    {
+                        Street = "Test",
+                        City = "Istanbul",
+                        ZipCode = "34841"
+                    }
+                },
+                new User
+                {
+                    Name = "Test3",
+                    Email = "test3@gmail.com",
+                    Address = new Address()
+                    {
+                        Street = "Test",
+                        City = "Istanbul",
+                        ZipCode = "34841"
+                    }
+                },
+            };
+    }
+}
