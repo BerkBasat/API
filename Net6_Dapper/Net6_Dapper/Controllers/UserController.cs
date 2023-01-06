82ccdaffb5d4ea57806a7baa90297da1a2603f85
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-01-06 14:06:17 +0300

    .

---
+++ Net6_Dapper/Net6_Dapper/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net6_Dapper.Models;
+using System.Data.SqlClient;
+
+namespace Net6_Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<User> users = await SelectAllUsers(connection);
+            return Ok(users);
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<User>> GetUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var user = await connection.QueryFirstAsync<User>("select * from users where id = @Id", new
+            {
+                Id = userId
+            });
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateUser(User user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into users (username, firstname, lastname, password) values (@UserName, @FirstName, @LastName, @Password)", user);
+            return Ok(await SelectAllUsers(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("update users set username = @UserName, firstname = @FirstName, lastname = @LastName, password = @Password where id = @Id", user);
+            return Ok(await SelectAllUsers(connection));
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int userId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from users where id = @Id", new
+            {
+                Id = userId
+            });
+            return Ok(await SelectAllUsers(connection));
+        }
+
+        private static async Task<IEnumerable<User>> SelectAllUsers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<User>("select * from users");
+        }
+    }
+}
---
+++ Net6_Dapper/Net6_Dapper/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Net6_Dapper.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
